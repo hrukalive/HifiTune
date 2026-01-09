b8c75c0fd0cc9df5e0aea5d85fbe90f3e4536c07
hrukalive <7337458+hrukalive@users.noreply.github.com>
2026-01-09 14:39:03 -0500

    Fix main editor include path

--- src/plugin/PluginEditor.h
+++ src/plugin/PluginEditor.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "ui/MainEditor.h"
+#include "../ui/MainEditor.h"
 
 class HifiTuneAudioProcessorEditor final : public juce::AudioProcessorEditor
 {
